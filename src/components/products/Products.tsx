deb723f5742729f2b5519d15a9641f71dde12b50
torogeldi <talantbekovtorogeldi0@gmail.com>
2024-04-18 17:01:29 +0600

    ended:style

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,20 +1,17 @@
 import React from "react";
 import "./products.css";
-import img from "../../assets/phone products.png";
-import { LiaStarSolid } from "react-icons/lia";
-import { IoCheckmark } from "react-icons/io5";
-import { IoIosClose } from "react-icons/io";
-import { CiHeart } from "react-icons/ci";
-import { IoStatsChartOutline } from "react-icons/io5";
+import img from "../../assets/iphone 13.png";
+import heart from "../../assets/Vector.png";
 import icon from "../../assets/export.png";
-import solid from "../../assets/Icon.png";
+import chat from "../../assets/chart.png";
+import { Link } from "react-router-dom";
 
 const Products: React.FC = () => {
   return (
     <section id="Productss">
       <div className="container">
         <div className="products">
-          <h1>Смартфоны</h1>
+          <h1 className="smart">Смартфоны</h1>
           <div className="title">
             <p>
               Все что нужно знать при выборе{" "}
@@ -24,115 +21,168 @@ const Products: React.FC = () => {
             </p>
           </div>
         </div>
-        <div className="Product">
-          <img className="Phone" src={img} alt="" />
-          <div className="Pretitle">
-            <div className="Pretext">
-              <div className="badgr">
-                <h1 className="otsenka">4.4 Оценка экпертов</h1>
-                <h1 className="othv">
-                  4.0 <img src={solid} alt="" />
-                  <img src={solid} alt="" />
-                  <img src={solid} alt="" />
-                  <img src={solid} alt="" />
-                  <LiaStarSolid /> 447 отзывов
+        {[1, 2, ].map((idx) => (
+          <div key={idx} className="groupOfProducts_content_objects_object">
+            <div className="groupOfProducts_content_objects_object_info_buttons1">
+                <h1 className="groupOfProducts_content_objects_object_info_advantage-name1">
+                  Apple iPhone 13 Pro Max 256 ГБ серый
                 </h1>
+                <h1 className="groupOfProducts_content_objects_object_info_advantage-price1">
+                  78 999 ₽
+                </h1>
+                <div>
+                  <button className="groupOfProducts_content_objects_object_info_button">
+                    <img src={heart} alt="" />
+                  </button>
+                  <Link
+                    to={"/differences"}
+                    className="groupOfProducts_content_objects_object_info_button"
+                  >
+                    <img src={chat} alt="no image" />
+                  </Link>
+                </div>
               </div>
-              <h4 className="icon">
-                <CiHeart className="icons" />{" "}
-                <IoStatsChartOutline className="icons" />
-              </h4>
+            <img
+              src={img}
+              alt=""
+              className="groupOfProducts_content_objects_object-img"
+            />
+            <div className="groupOfProducts_content_objects_object_info">
+              <div className="groupOfProducts_content_objects_object_info_buttons">
+                <h1 className="groupOfProducts_content_objects_object_info_advantage-name1">
+                  Apple iPhone 13 Pro Max 256 ГБ серый
+                </h1>
+                <h1 className="groupOfProducts_content_objects_object_info_advantage-price1">
+                  78 999 ₽
+                </h1>
+                <div>
+                  <button className="groupOfProducts_content_objects_object_info_button">
+                    <img src={heart} alt="" />
+                  </button>
+                  <Link
+                    to={"/differences"}
+                    className="groupOfProducts_content_objects_object_info_button"
+                  >
+                    <img src={chat} alt="no image" />
+                  </Link>
+                </div>
+              </div>
+              <div className="groupOfProducts_content_objects_object_info_advantage">
+                <div className="groupOfProducts_content_objects_object_info_advantage_title">
+                  <h1 className="groupOfProducts_content_objects_object_info_advantage-name">
+                    Apple iPhone 13 Pro Max 256 ГБ серый
+                  </h1>
+                  <h1 className="groupOfProducts_content_objects_object_info_advantage-price">
+                    78 999 ₽
+                  </h1>
+                </div>
+                <div className="groupOfProducts_content_objects_object_info_advantage_info">
+                  <div className="groupOfProducts_content_objects_object_info_advantage_info_block1">
+                    <h1 className="groupOfProducts_content_objects_object_info_advantage_info_block1-text">
+                      ✓ Dual Sim, 3G, 4G, 5G, VoLTE, Wi-Fi, NFC
+                    </h1>
+                    <h1 className="groupOfProducts_content_objects_object_info_advantage_info_block1-text">
+                      ✓ 6.1 inches, 1170 x 2532 px Display with Small Notch
+                    </h1>
+                    <h1 className="groupOfProducts_content_objects_object_info_advantage_info_block1-text">
+                      ✓ Bionic A15, Hexa Core, 3.22 GHz Processor
+                    </h1>
+                    <h1 className="groupOfProducts_content_objects_object_info_advantage_info_block1-text">
+                      ✓ 12 MP + 12 MP Dual Rear & 12 MP Front Camera
+                    </h1>
+                  </div>
+                  <div className="groupOfProducts_content_objects_object_info_advantage_info_block2">
+                    <h1 className="groupOfProducts_content_objects_object_info_advantage_info_block1-text">
+                      ✓ 4 GB RAM, 128 GB inbuilt
+                    </h1>
+                    <h1 className="groupOfProducts_content_objects_object_info_advantage_info_block1-text">
+                      ✕ Memory Card Not Supported
+                    </h1>
+                    <h1 className="groupOfProducts_content_objects_object_info_advantage_info_block1-text">
+                      ✓ 3240 mAh Battery with Fast Charging
+                    </h1>
+                    <h1 className="groupOfProducts_content_objects_object_info_advantage_info_block1-text">
+                      ✓ iOS v15
+                    </h1>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="Iphones">
-              <h1 className="Iphone">Apple iPhone 13 Pro Max 256 ГБ серый</h1>
-              <h1 className="price">
-                13% <span>78 999 ₽</span>{" "}
-              </h1>
-            </div>
-            <div className="Chracters">
-              <li className="charakter">
-                <IoCheckmark />
-                Dual Sim, 3G, 4G, 5G, VoLTE, Wi-Fi, NFC
-              </li>
-              <li className="charakter">
-                <IoCheckmark />4 GB RAM, 128 GB inbuilt
-              </li>
-              <li className="charakter">
-                <IoCheckmark />
-                6.1 inches, 1170 x 2532 px Display with Small Notch
-              </li>
-              <li className="charakter">
-                <IoIosClose />
-                Memory Card Not Supporteds
-              </li>
-              <li className="charakter">
-                <IoCheckmark />
-                Bionic A15, Hexa Core, 3.22 GHz Processor
-              </li>
-              <li className="charakter">
-                <IoCheckmark />
-                3240 mAh Battery with Fast Charging
-              </li>
-              <li className="charakter">
-                <IoCheckmark />
-                12 MP + 12 MP Dual Rear & 12 MP Front Camera
-              </li>
-              <li className="charakter">
-                <IoCheckmark />
-                iOS v15
-              </li>
+            <div className="groupOfProducts_content_objects_object_characteristics">
+              <div className="phone_info_infos ">
+                <div className="phone_info_info">
+                  <span>Дизайн</span>
+                  <div className="phone_info_lines">
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line phone_info_line_empty"></div>
+                  </div>
+                </div>
+                <div className="phone_info_info">
+                  <span>Батарея</span>
+                  <div className="phone_info_lines">
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line phone_info_line_empty"></div>
+                    <div className="phone_info_line phone_info_line_empty"></div>
+                    <div className="phone_info_line phone_info_line_empty"></div>
+                  </div>
+                </div>
+                <div className="phone_info_info">
+                  <span>Дисплей</span>
+                  <div className="phone_info_lines">
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line phone_info_line_empty"></div>
+                  </div>
+                </div>
+                <div className="phone_info_info">
+                  <span>Камера</span>
+                  <div className="phone_info_lines">
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                  </div>
+                </div>
+                <div className="phone_info_info">
+                  <span>Ответ</span>
+                  <div className="phone_info_lines">
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                  </div>
+                </div>
+                <div className="phone_info_info">
+                  <span>Портативность</span>
+                  <div className="phone_info_lines">
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line"></div>
+                    <div className="phone_info_line phone_info_line_empty"></div>
+                  </div>
+                </div>
+              </div>
+              <button className="groupOfProducts_content_objects_object_characteristics_button">
+                Перейти к товару
+              </button>
             </div>
           </div>
-          <div className="Fail">
-            <h1 className="frame">дизайн</h1>
-            <div className="border">
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-            </div>
-            <h1 className="frame">Батарея</h1>
-            <div className="border">
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-            </div>
-            <h1 className="frame">дисплей</h1>
-            <div className="border">
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-            </div>
-            <h1 className="frame">камера</h1>
-            <div className="border">
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-            </div>
-            <h1 className="frame">ответ</h1>
-            <div className="border">
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-            </div>
-            <h1 className="frame">Портативность</h1>
-            <div className="border">
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-              <div className="bad"></div>
-            </div>
-            <button className="btn">Перей к товару</button>
+        ))}
+        <div className="productss">
+          <h1 className="text"> Показ 1 – 10 из 1286</h1>
+          <div className="title2">
+            <p className="pre">
+              назад <span className="tapi">1 2 3 4 5 ... 51</span>вперед
+            </p>
           </div>
         </div>
       </div>
