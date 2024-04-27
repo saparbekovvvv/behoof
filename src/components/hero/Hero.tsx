989d9362a43812970ae621660b6f38025a1d65c3
ayana <aanabaktybekova97@gmail.com>
2024-04-27 16:33:32 +0600

    fixed

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -5,26 +5,6 @@ function Hero() {
     return (
         <section id="Hero">
             <div className="container">
-<<<<<<< HEAD
-                <div className="main">
-                    <div className="hero">
-                        <div className="hero-content">
-                            <p>
-                                <span>1.8 млн</span> товаров в <span>2272</span>{" "}
-                                магазинах <br /> найди, сравни, выберай!
-                            </p>
-                            <NavLink to="/products">
-                                <button>Перейти к категориям</button>
-                            </NavLink>
-                        </div>
-                        <div className="hero-content-two">
-                            <p>
-                                <span>Топ-10 </span> смартфонов 2023 года
-                            </p>
-                            <button>Смотреть</button>
-                            <img src={phone} alt="" />
-                        </div>
-=======
                 <div className="hero">
                     <div className="hero-content">
                         <p>
@@ -41,7 +21,6 @@ function Hero() {
                         </p>
                         <button>Смотреть</button>
                         <img src={phone} alt="" />
->>>>>>> 7e0de1e203051d470d1e84f265a9a2f21b0ae4e6
                     </div>
                 </div>
             </div>
